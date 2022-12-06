084badf4d9efbf7063408dd53048564a196f9c02
denismahmutovic <djdeny95@gmail.com>
2022-12-06 18:17:30 +0100

    movie

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { HomePageVideos } from "../Types";
 
 export default function ({ data }: { data: HomePageVideos }) {
-  //   const isData = data ? true : false;
-  return <div> card </div>;
+  return (
+    <div className="w-94 h-60 flex gap-3 flex-col">
+      <div className="relative">
+        <span className=" absolute bottom-3 right-3 text-sm bg-gray-900 px-2 py-0.5 z-10">
+          {data.videoDuration}
+        </span>
+        <Link to={`/watch/${data.videoId}`}>
+          <img
+            src={data.videoThumbnail}
+            className="h-44 w-72"
+            alt="Thumbnail"
+          />
+        </Link>
+      </div>
+      <div className="flex-gap-2">
+        <div className="min-w-fit">
+          <a href="#">
+            <img
+              src={data.channelInfo.image}
+              alt="channel"
+              className="h-9 w-9 rounded-full"
+            />
+          </a>
+        </div>
+        <div>
+          <h3>
+            <a href="#" className="line-clamp-2">
+              {data.videoTitle}
+            </a>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 }
