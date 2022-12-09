c36817527aa82a3298d2e95030dd27594872b9c9
denismahmutovic <djdeny95@gmail.com>
2022-12-09 19:57:13 +0100

    search

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,6 +1,34 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { getHomePageVideos } from "../store/reducers/getHomePagaVideos";
 
 export default function Watch() {
+  const [showMoreStatus, setShowMoreStatus] = useState<boolean>(false);
+  const { id } = useParams();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const currentPlaying = useAppSelector(
+    (state) => state.youtubeApp.currentPlaying
+  );
+  const recommendedVideos = useAppSelector(
+    (state) => state.youtubeApp.recommendedVideos
+  );
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getHomePageVideos(id));
+      setShowMoreStatus(false);
+    } else {
+      navigate("/");
+    }
+  }, [id, navigate, dispatch]);
+
+  useEffect(() => {
+    if (currentPlaying && id) dispatch(getRecommendedVideos(id));
+  }, [currentPlaying, dispatch, id]);
+
   return <div>Watch</div>;
 }
